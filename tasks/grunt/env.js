04d724eab26c88cca27fb31d7393b05dfd32df9c
Chris Hourihan <chourihan@yikyakapp.com>
2015-05-26 14:52:46 -0400

    initial commit

---
+++ tasks/grunt/env.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // Set your environments
+  dev: {
+    NODE_ENV: 'localhost'
+  },
+  test: {
+    NODE_ENV: 'test'
+  },
+  staging: {
+    NODE_ENV: 'staging'
+  },
+  prod: {
+    NODE_ENV: 'production'
+  },
+};
---
+++ tasks/grunt/jshint.js
@@ -0,0 +1,10 @@
+module.exports = {
+  options: {
+    jshintrc: '.jshintrc'
+  },
+  files: [
+    // Specify what to lint (and !not lint)
+    'lib/**/*.js',
+    'index.js'
+  ]
+};
